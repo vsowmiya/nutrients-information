d377a906c08d5a7094f349f6c07fee46c934ffde
vsowmiya <vsowmi123@gmail.com>
2013-07-15 21:13:56 +0530

    Create veg.java

---
+++ veg.javapackage-com.example.projectn-import-com.example.projectn.R-import-com.example.projectn.R.layout-import-com.example.projectn.R.menu-import-android.os.Bundle-import-android.app.Activity-import-android.content.Intent-import-android.view.Menu-import-android.view.View-public-class-fru-extends-Activity-Override-protected-void-onCreate-Bundle-savedInstanceState-super.onCreate-savedInstanceState-setContentView-R.layout.fru-Override-public-boolean-onCreateOptionsMenu-Menu-menu/veg.java
@@ -0,0 +1,213 @@
+package com.example.projectn;
+
+
+
+import com.example.projectn.R;
+import com.example.projectn.R.layout;
+import com.example.projectn.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+
+public class fru extends Activity {
+
+
+  @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fru);
+	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_projectn, menu);
+		return true;
+	}
+	public void clickfruit(View v)
+	{
+		setContentView(R.layout.activity_projectn);
+		}
+	public void clickap(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINAPP" ));
+	}
+	public void clickav(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINAVO" ));
+	}
+	public void clickban(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINBAN" ));
+	}
+	public void clickblack(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINBLACK" ));
+	}
+	public void clickblue(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINBLUE" ));
+	}
+	public void clickboy(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINBOY" ));
+	}
+	public void clickbr(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINBRE" ));
+	}
+	public void clickcan(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINCAN" ));
+	}
+	public void clickbl(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINBLA" ));
+	}
+	public void clickcheri(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINCHERI" ));
+	}
+	public void clickche(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINCHE" ));
+	}
+	public void clickchin(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINCHIN" ));
+	}
+	public void clickcran(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINCRAN" ));
+	}
+	public void clickdate(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINDATE" ));
+	}
+	public void clickfig(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINFIG" ));
+	}
+	public void clickgoose(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINGOOSE" ));
+	}
+	public void clickgra(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINGRA" ));
+	}
+	public void clickgrape(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINGRAPE" ));
+	}
+	public void clickgua(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINGUA" ));
+	}
+	public void clickkiwi(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINKIW" ));
+	}
+	public void clicklem(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINLEM" ));
+	}
+	public void clicklime(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINLIME" ));
+	}
+	public void clicklogen(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINLOGAN" ));
+	}
+	public void clicklyc(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINLYC" ));
+	}
+	public void clickman(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINMAN" ));
+	}
+	public void clickmul(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINMUL" ));
+	}
+	public void clicknec(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINNEC" ));
+	}
+	public void clickoli(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINOLI" ));
+	}
+	public void clickoran(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINORG" ));
+	}
+	public void clickpap(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINPAP" ));
+	}
+	public void clickpas(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINPAS" ));
+	}
+	public void clickpeac(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINPEAC" ));
+	}
+	public void clickpear(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINPEAR" ));
+	}
+	public void clickpine(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINPIN" ));
+	}
+	public void clickper(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINPER" ));
+	}
+	public void clickplu(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINPLU" ));
+	}
+	public void clickpom(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINPOM" ));
+	}
+	public void clickpri(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINPRIC" ));
+	}
+	public void clickrais(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINRAIS" ));
+	}
+	public void clickrasp(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINRASP" ));
+	}
+	public void clickstar(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINSTAR" ));
+	}
+	public void clickstraw(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINSTRAW" ));
+	}
+	public void clicktom(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINTOM" ));
+	}
+	public void clickwat(View v)
+	{
+		startActivity(new Intent("android.intent.action.MAINWATER" ));
+	}
+	
+}
